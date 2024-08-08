bfc134e83d3cc1b70b29ba1546860e2cd6cec010
Hadi Alhadi <alhadi.h1993@gmail.com>
2024-08-08 22:13:25 +0400

    Initial commit

---
+++ auth/src/index.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import {json} from "body-parser";
+import {currentUserRouter} from "./routes/current-user";
+import {signinRouter} from "./routes/signin";
+import {signoutRouter} from "./routes/signout";
+import {signupRouter} from "./routes/signup";
+import {errorHandler} from "./middlewares/error-handler";
+import mongoose from "mongoose";
+
+const app = express();
+app.use(json());
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.use(errorHandler)
+
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.log(error);
+    }
+
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000!');
+    });
+}
+
+start();
+
